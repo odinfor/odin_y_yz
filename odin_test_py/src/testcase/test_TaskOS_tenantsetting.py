27399d6d3a73faeeb004d3c0fead0a66a1f807f7
odin <747069227@qq.com>
2018-11-23 09:52:57 +0800

    updata

---
+++ odin_test_py/src/testcase/test_TaskOS_tenantsetting.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/11/23 上午9:51
+# @Author  : odin_y
+# @Site    : 
+# @File    : test_TaskOS_tenantsetting.py
+# @Comment : 租户管理任务配置接口
+
+import unittest
